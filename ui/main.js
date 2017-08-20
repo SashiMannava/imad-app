eadaaebe75135c3bcf0650974d17f1fb100da6bd
SashiMannava <sashidhar.mannava@gmail.com>
2017-08-20 21:55:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,13 @@ element.innertHTML = 'New value';
 
 // Move the Image
 var img = document.getElementById("madi");
+var marginLeft = 0;
+funtion moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+    }
 
 img.onclick = function () {
-    img.style.marginleft = '100px';
+    var interval = setinterval(moveRight, 100);
+// img.style.marginleft = '100px';
 };
