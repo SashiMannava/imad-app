6af95f6af14953c27e2330255fc47862782df468
SashiMannava <sashidhar.mannava@gmail.com>
2017-08-21 15:19:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,12 +17,23 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onClick = function() {
     //make a request to the endpoint
-    
+    var request = new XMLHttpRequest();
     //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if (request.readyState) = XMLHttpRequest.DONE {
+        //Take some action
+            if (request.status) = 200
+            var counter = request.responseText;
+            }
+        };
+    };
+        // not done yet
+        
+        
     
     //render the variable in the correct span
     counter = counter + 1;
-var span = document.getElementById('count');
+    var span = document.getElementById('count');
     span.innerHTML = counter.toString();
     
 };
