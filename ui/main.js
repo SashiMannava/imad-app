459d6403cb6d33c86bd07338ca44293665423a9a
SashiMannava <sashidhar.mannava@gmail.com>
2017-08-21 15:40:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,23 +14,25 @@
 //};
 //Counter code
 var button = document.getElementById('counter');
+
 button.onClick = function() {
     //Create a request object
     var request = new XMLHttpRequest();
+    
     //capture the response and store it in a variable
     request.onreadystatechange = function() {
-        if (request.readyState=== XMLHttpRequest.DONE) {
+        if (request.readyState === XMLHttpRequest.DONE) {
         //Take some action
-            if (request.status === 200);
+            if (request.status === 200) {
             var counter = request.responseText;
             var span = document.getElementById('count');
             span.innerHTML = counter.toString();
             }
-        };
+        }
     };
         // not done yet
         
     //make the request
     request.open('GET', 'http://sashidharmannava.imad.hasura-app.io/counter', true);
     request.send(null);
- };
+};
