f2f2a7b00363fa8dc18cfbb9f194cfe7a09d5a4e
SashiMannava <sashidhar.mannava@gmail.com>
2017-08-21 14:14:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 var counter=0
 app.get('/counter', function(req, res) {
     counter = counter + 1;
-    res.send(counter, toString())
+    res.send(counter.toString())
 });
 app.get('/Article-one', function(req, res) {
     res.send('Article one is requested and will be served here');
