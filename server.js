55093097569c2563e734d56ddeed3daf8c543798
SashiMannava <sashidhar.mannava@gmail.com>
2017-08-21 14:15:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var counter=0
+var counter=0;
 app.get('/counter', function(req, res) {
     counter = counter + 1;
-    res.send(counter.toString())
+    res.send(counter.toString());
 });
 app.get('/Article-one', function(req, res) {
     res.send('Article one is requested and will be served here');
